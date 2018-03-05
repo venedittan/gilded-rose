c6a32bc4bcd5fc604b8c7644f57d57ff31b959a0
ryhita <tahiry.andriana@gmail.com>
2018-03-05 20:30:21 +0100

    Test item

--- src/main/java/fr/esiea/GildedRose.java
+++ src/main/java/fr/esiea/GildedRose.java
@@ -8,11 +8,7 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) 
-            updateOneItem(items[i]);
-    }
-
-    private void updateOneItem(Item item) {
+        for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
@@ -62,6 +58,5 @@ class GildedRose {
                 }
             }
         }
-
     }
 }
