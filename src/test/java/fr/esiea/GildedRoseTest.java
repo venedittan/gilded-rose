6eeea553ed832ae975e9107631d247c52bd3c90b
ryhita <tahiry.andriana@gmail.com>
2018-03-05 18:29:27 +0100

    Modif Fichiers Java

--- src/test/java/fr/esiea/GildedRoseTest.java
+++ src/test/java/fr/esiea/GildedRoseTest.java
@@ -8,10 +8,7 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+       
     }
 
 }
