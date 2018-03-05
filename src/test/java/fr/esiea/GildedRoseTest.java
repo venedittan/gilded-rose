ef21c01013ccc0a476233c94767a381574a1ba69
ryhita <tahiry.andriana@gmail.com>
2018-03-05 21:04:21 +0100

    Test degradation objet

--- src/test/java/fr/esiea/GildedRoseTest.java
+++ src/test/java/fr/esiea/GildedRoseTest.java
@@ -9,7 +9,7 @@ public class GildedRoseTest {
     @Test
     public void updateQualityTest() {
 		Item item = new Item("Item Test",10,10);
-		Item[] itemsT= new Item[] {item};
+		Item[] items= new Item[] {item};
 		GildedRose gildedRose = new GildedRose(items);
 		gildedRose.updateQuality();
 		assertEquals(9, item.getQuality());
