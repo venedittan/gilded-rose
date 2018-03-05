7dab5135d7d72326734e534ebc4b4108833e9304
ryhita <tahiry.andriana@gmail.com>
2018-03-05 20:48:00 +0100

    Test item

--- src/test/java/fr/esiea/GildedRoseTest.java
+++ src/test/java/fr/esiea/GildedRoseTest.java
@@ -9,7 +9,9 @@ public class GildedRoseTest {
     @Test
     public void updateQualityTest() {
 		Item item = new Item("Item Test",10,10);
-		updateQuality();
+		Item[] itemsT= new Item[] {item};
+		GildedRose gildedRose = new GildedRose(items);
+		gildedRose.updateQuality();
 		assertEquals(9, item.getQuality());
     }
 
