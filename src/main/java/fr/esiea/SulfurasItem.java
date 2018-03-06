d9bfe2f121cf05d96edae0f7c52c32668a139b73
ryhita <tahiry.andriana@gmail.com>
2018-03-06 02:31:21 +0100

    Modif Classes Java

--- src/main/java/fr/esiea/SulfurasItem.java
+++ src/main/java/fr/esiea/SulfurasItem.java
@@ -5,7 +5,7 @@ import java.lang.String;
 public class SulfurasItem extends Item {
 
 	public SulfurasItem(String name, int sellIn, int quality) {
-		super(name,sellIn);
+		super(name,sellIn,quality);
 	}
 
 	public void updateSellIn() {
