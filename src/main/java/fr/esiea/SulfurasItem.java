5192e7449ae8fac6b229999ab7c4b83dcb876bf9
ryhita <tahiry.andriana@gmail.com>
2018-03-06 01:35:02 +0100

    Modif Classes Java

--- src/main/java/fr/esiea/SulfurasItem.java
+++ src/main/java/fr/esiea/SulfurasItem.java
@@ -2,7 +2,7 @@ package fr.esiea;
 
 public class SulfurasItem extends Item {
 
-	public SulfuraItem(String name, int sellIn, int quality) {
+	public SulfurasItem(String name, int sellIn, int quality) {
 		this.name = "Sulfuras, Hand of Ragnaros";
 		super(sellIn);
 		this.quality = 80;
