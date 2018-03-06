9da478a96b26a5692bf9c0b874b1a30b9a3a86e5
ryhita <tahiry.andriana@gmail.com>
2018-03-06 03:35:59 +0100

    Modif Classes Java

--- src/test/java/fr/esiea/ItemTest.java
+++ src/test/java/fr/esiea/ItemTest.java
@@ -19,5 +19,22 @@ public class ItemTest {
     	agedBrieItem.updateQuality();
     	assertEquals(12, agedBrieItem.quality);
   	}
+
+  	@Test
+  	public void updateItemTest(){
+    	Item[] items= new Item[] { 
+    		new AgedBrieItem("Aged Brie", -5, 10),
+     		new BackstagePassesItem("Backstage passes to a TAFKAL80ETC concert",10,20),
+    		new BackstagePassesItem("Backstage passes to a TAFKAL80ETC concert",5,10),
+     		new BackstagePassesItem("Backstage passes to a TAFKAL80ETC concert",-2,5),
+     		new ConjuredItem("Conjured",-4,2)};
+     	for(i=0;i<5;i++)
+     		items[i].updateQuality();
+     	assertEquals(12, items[0].quality);
+    	assertEquals(21, items[1].quality);
+   		assertEquals(11, items[2].quality);
+    	assertEquals(0, items[3].quality);
+    	assertEquals(0, items[4].quality);
+    }
     
 }
