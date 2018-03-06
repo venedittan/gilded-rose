1374db29405974fd805d190968aeea6c725a4167
ryhita <tahiry.andriana@gmail.com>
2018-03-06 02:10:46 +0100

    Modif Classes Java

--- src/main/java/fr/esiea/ConjuredItem.java
+++ src/main/java/fr/esiea/ConjuredItem.java
@@ -3,7 +3,7 @@ package fr.esiea;
 public class ConjuredItem extends Item {
 
 	public ConjuredItem(String name, int sellIn, int quality) {
-		super(int sellIn, int quality);
+		super(sellIn, quality);
 		this.name = "Conjured";
 	}
 
